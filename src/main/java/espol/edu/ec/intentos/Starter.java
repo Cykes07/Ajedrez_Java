310041696da8bcc02ce12782dd68bb38bd640393
Cykes <>
2023-09-04 21:14:33 -0500

    Cambio para crear el .jar

---
+++ src/main/java/espol/edu/ec/intentos/Starter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package espol.edu.ec.intentos;
+
+/**
+ *
+ * @author Cykes
+ */
+public class Starter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        App.main(args);
+    }
+    
+}
